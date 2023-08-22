b55944cd7e06cfac9cd0b725f2870b2739023b27
zhiqianghou <zhiqiang.hou.ibp@gmail.com>
2023-08-22 18:08:12 -0500

    Add back-end coding for streching data from openweathermap api

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+from back_end import get_data
 
 
 st.title("Weather Forecast for the next Days")
@@ -10,8 +11,7 @@ option = st.selectbox("Select data type to view",
 					  ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-dates = ["2023-08-01", "2023-08-02", "2023-08-03"]
-temperatures = [10, 20, 30]
+data = get_data()
 figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y":"Temperater(C)"})
 
 st.plotly_chart(figure)
