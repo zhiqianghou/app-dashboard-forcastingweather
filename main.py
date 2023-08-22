9b762adfdb9800fd4cf2a25091a4712d1f33f04d
zhiqianghou <zhiqiang.hou.ibp@gmail.com>
2023-08-22 17:28:42 -0500

    Plot hard-coded data

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import plotly.express as px
+
 
 st.title("Weather Forecast for the next Days")
 place = st.text_input("Place:")
@@ -7,6 +9,12 @@ days = st.slider("Forcast Days:", min_value=1, max_value= 5,
 option = st.selectbox("Select data type to view",
 					  ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
+
+dates = ["2023-08-01", "2023-08-02", "2023-08-03"]
+temperatures = [10, 20, 30]
+figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y":"Temperater(C)"})
+
+st.plotly_chart(figure)
 
 
 
